46d6055eb6dce1695b87894413d81ccdf623d976
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2020-02-10 17:36:52 -0300

    wiring up redux-thunk and calling external api service

--- api-call-app/src/components/App.js
+++ api-call-app/src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PostList from '../components/PostList.js'
 
 const App = () => {
-  return ( <div className="ui container">App</div> );
+  return (
+    <div className="ui container">
+      <PostList/>
+    </div>
+  );
 }
  
 export default App;
